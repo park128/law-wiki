54b0e28d75d45ec925973fd8cf17517312587272
park128 <rmsxo12816@gmil.com>
2022-08-09 16:09:22 +0900

    intitial updated

---
+++ app.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+import pandas as pd 
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+## DB 연결 Local
+def db_create():
+    # 로컬
+    engine = create_engine("postgresql://postgres:1234@localhost:5432/chatbot", echo = False)
+		
+		# Heroku
+    engine = create_engine("postgresql://mzgnoxixyinhjd:d7ac5c7c91e4b4b82bfbdc56dc09a762cf9025423f26c0d0d5d0a6a1a6442765@ec2-34-235-31-124.compute-1.amazonaws.com:5432/dfqa3qaa76tclf", echo = False)
+
+    engine.connect()
+    engine.execute("""
+        CREATE TABLE IF NOT EXISTS iris(
+            sepal_length FLOAT NOT NULL,
+            sepal_width FLOAT NOT NULL,
+            pepal_length FLOAT NOT NULL,
+            pepal_width FLOAT NOT NULL,
+            species VARCHAR(100) NOT NULL
+        );"""
+    )
+    data = pd.read_csv('data/iris.csv')
+    print(data)
+    data.to_sql(name='iris', con=engine, schema = 'public', if_exists='replace', index=False)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    db_create()
+    return "Hello World!"
+
+
+
+if __name__ == "__main__":
+    db_create()
+    app.run()
