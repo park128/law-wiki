71534bdc1191ce48b125df5ede55e142133697f8
park128 <rmsxo12816@gmil.com>
2022-08-30 19:45:55 +0900

    updated

--- countingstars.py
+++ countingstars.py
@@ -3,7 +3,7 @@ import os,sys, json
 import pandas as pd 
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 import numpy as np
-import psycopg2 as db
+import psycopg2
 import pandas as pd
 import countingstars
 #데이터 베이스를 불러온다
@@ -12,14 +12,15 @@ def namecheck(nick_name):
     #대충 여기서 있는지 없는지를 확인한다.
     #불러와서 확인하는 방법으로 그리고 정렬까지
     conn_string="dbname='ddtk33j69v200c' host='ec2-54-225-234-165.compute-1.amazonaws.com' user='uxweficayqkvnb' password='191795f6687a563f2d49dd25fa1d4a3b481604b2bfb416f11811f430377a463f'"
-    conn=db.connect(conn_string)
+    conn=psycopg2.connect(conn_string)
     script = 'SELECT * FROM public."user"'
     df = pd.read_sql(script, conn)
     print(df.head())
     cursor = conn.cursor()
     sql = "SELECT * FROM user;"
-    result = cursor.excute(sql)
-    result = pd.DataFrame(result)
+    cursor.excute(sql)
+    rows = cursor.fetchall()
+    result = pd.DataFrame(rows)
     #불러왔다.
     if result.find(nickname) == -1:
         sql = "INSERT INTO user(이름, 점수) VALUES ("+ nickname + ", 0)"
