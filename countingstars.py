e7ec5e59a25ae7ba1e95c94a0e9bd45c883a235d
park128 <rmsxo12816@gmil.com>
2022-08-30 19:37:24 +0900

    updated

--- countingstars.py
+++ countingstars.py
@@ -6,9 +6,11 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 def namecheck(nick_name):
     #대충 여기서 있는지 없는지를 확인한다.
     #불러와서 확인하는 방법으로 그리고 정렬까지
-    lawwiki_db = engine.connect(host='ec2-54-225-234-165.compute-1.amazonaws.com', port= 5432, user='uxweficayqkvnb', password='191795f6687a563f2d49dd25fa1d4a3b481604b2bfb416f11811f430377a463f', db='ddtk33j69v200c',\
-                          charset='utf8mb4')
-    cursor = lawwiki_db.cursor(pymysql.cursor.DictCursor)
+    conn_string="dbname='ddtk33j69v200c' host='ec2-54-225-234-165.compute-1.amazonaws.com' user='uxweficayqkvnb' password='191795f6687a563f2d49dd25fa1d4a3b481604b2bfb416f11811f430377a463f'"
+    conn=db.connect(conn_string)
+    script = 'SELECT * FROM public."user"'
+    df = pd.read_sql(script, conn)
+    print(df.head())
 
     sql = "SELECT * FROM user;"
     result = cursor.excute(sql)
