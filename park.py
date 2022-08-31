b8de5a3e81bbe05dcc80e5f81b97560d580af56e
park128 <rmsxo12816@gmil.com>
2022-08-31 15:18:36 +0900

    pdated

--- park.py
+++ park.py
@@ -6,10 +6,9 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 ## DB 연결 Local
 def db_create():
     # 로컬
-		engine = create_engine("postgresql://postgres:1234@localhost:5432/chatbot", echo = False)
-		
-		# Heroku
-		engine = create_engine("postgresql://uxweficayqkvnb:191795f6687a563f2d49dd25fa1d4a3b481604b2bfb416f11811f430377a463f@ec2-54-225-234-165.compute-1.amazonaws.com:5432/ddtk33j69v200c", echo = False)
+    
+	# Heroku
+    engine = create_engine("postgresql://uxweficayqkvnb:191795f6687a563f2d49dd25fa1d4a3b481604b2bfb416f11811f430377a463f@ec2-54-225-234-165.compute-1.amazonaws.com:5432/ddtk33j69v200c", echo = False)
 
     engine.connect()
     engine.execute("""
